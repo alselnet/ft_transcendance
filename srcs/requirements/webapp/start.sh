625832413aba7a3f8bdad0e1675158c22b8b2b4d
alselnet <alex.selnet@gmail.com>
2024-05-07 17:47:55 +0200

    added superuser rule

--- srcs/requirements/webapp/start.sh
+++ srcs/requirements/webapp/start.sh
@@ -4,4 +4,6 @@
 echo "Database is ready"
 env/bin/python3 manage.py makemigrations
 env/bin/python3 manage.py migrate
+env/bin/python3 manage.py createsuperuser --username=admin --email='' --no-input
+env/bin/python3 manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username='admin'); user.set_password('admin'); user.save()"
 env/bin/python3 manage.py runserver 0.0.0.0:8000
