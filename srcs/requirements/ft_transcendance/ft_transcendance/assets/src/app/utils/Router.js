84958df2e481de4bcac9b84e404125b24414d0c1
Jeremy Cointre <jeremycointre@Jeremys-MacBook-Air.local>
2024-06-14 13:34:09 +0200

    added new .js files and updated router.js

--- srcs/requirements/ft_transcendance/ft_transcendance/assets/src/app/utils/Router.js
+++ srcs/requirements/ft_transcendance/ft_transcendance/assets/src/app/utils/Router.js
@@ -3,6 +3,9 @@ import LogIn from "../pages/LogIn.js";
 import SignIn from "../pages/SignIn.js";
 import Game from "../pages/Game.js";
 import Dashboard from "../pages/Dashboard.js"
+import GameHistory from "./game-history.js";
+import Settings from "./settings.js";
+import TwoFactorAuth from "./2FA.js";
 
 //définition des routes et la gestion des changements d'URL
 export const Router = () => {
@@ -26,6 +29,12 @@ export const Router = () => {
             return Game();
         case "#/dashboard":
             return Dashboard();
+        case "#/game-history":
+            return GameHistory();
+        case "#/settings":
+            return Settings();
+        case "#/2FA":
+            return TwoFactorAuth();
         default:
             document.getElementById("section").innerHTML = "<h1>Page not found</h1>";
             break;
