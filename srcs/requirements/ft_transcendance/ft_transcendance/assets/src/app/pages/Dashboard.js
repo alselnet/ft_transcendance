7d6e5da5d81df51af2f30fef03eab92687f91f68
Jeremy Cointre <jeremycointre@Jeremys-MacBook-Air.local>
2024-06-23 03:29:24 +0200

    jwt in dashboard.js

--- srcs/requirements/ft_transcendance/ft_transcendance/assets/src/app/pages/Dashboard.js
+++ srcs/requirements/ft_transcendance/ft_transcendance/assets/src/app/pages/Dashboard.js
@@ -6,6 +6,28 @@ import { showCircle } from "../animation/ShowCircle.js";
 import Leaderboard from "./Leaderboard.js";
 
 export const Dashboard = () => {
+    // Vérifie l'authentification de l'utilisateur
+    const checkAuthentication = () => {
+        const token = localStorage.getItem('accessToken');
+        if (!token) {
+            window.location.href = '#/connexion'; // Redirige vers la page de login si pas de token
+            return false;
+        } else {
+            const payload = JSON.parse(atob(token.split('.')[1])); // Décodage du payload du token
+            const expiry = payload.exp * 1000; // Convertir le temps d'expiration en millisecondes
+            if (Date.now() >= expiry) {
+                // Si le token est expiré, supprimer et rediriger
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('refreshToken');
+                window.location.href = '#/connexion';
+                return false;
+            }
+        }
+        return true;
+    };
+
+    if (!checkAuthentication()) return; // Arrêter le chargement de la page si l'utilisateur n'est pas authentifié
+    
     let divRoot = document.querySelector("#root");
 
     divRoot.innerHTML = "";
