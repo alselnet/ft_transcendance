980f730f118fdb4830d01ee0c61e8a704e27a3a9
TasMj <tasnim.mej@hotmail.com>
2024-07-01 11:17:59 +0100

    page about us added

--- srcs/requirements/ft_transcendance/ft_transcendance/assets/src/app/pages/AboutUs.js
+++ srcs/requirements/ft_transcendance/ft_transcendance/assets/src/app/pages/AboutUs.js
@@ -5,7 +5,49 @@ const AboutUs = () => {
     if (section) {
         section.innerHTML = 
     `
-        <h1>About Us</h1>
+	    <div class="main-container-aboutus">
+
+        <h1 class="title-aboutus">Qui sommes nous ?</h1>
+	    <div class="container-aboutus">
+	    	<div class="row">
+	    		<div id="block1" class="col-md-6 col-lg-4 mb-3">
+	    			<div class="frame-aboutus">
+	    				<h1 class="name-cube">Jéremy COINTRE</h1>
+	    				<img class="img-us" src="./app/images/profile.png" alt="img-jeremy">
+	    				<p>Développement du frontend</p>
+	    			</div>
+	    		</div>
+	    		<div id="block2" class="col-md-6 col-lg-4 mb-3">
+	    			<div class="frame-aboutus">
+	    				<h1 class="name-cube">Hugo COULON</h1>
+	    				<img class="img-us" src="./app/images/hugo.png" alt="img-hugo">
+	    				<p>Développement du jeu</p>
+	    			</div>
+	    		</div>
+	    		<div id="block3" class="col-md-6 col-lg-4 mb-3">
+	    			<div class="frame-aboutus">
+	    				<h1 class="name-cube">Tasnim MEJRI</h1>
+	    				<img class="img-us" src="./app/images/Tasnim.jpg" alt="img-tasnim">
+	    				<p>Développement du frontend</p>
+	    			</div>
+	    		</div>
+	    		<div id="block4" class="col-md-6 col-lg-4 mb-3">
+	    			<div class="frame-aboutus">
+	    				<h1 class="name-cube">Alexandre SELNET</h1>
+	    				<img class="img-us" src="./app/images/profile.png" alt="img-alex">
+	    				<p>Développement du Backend</p>
+	    			</div>
+	    		</div>
+	    		<div id="block5" class="col-md-6 col-lg-4 mb-3">
+	    			<div class="frame-aboutus">
+	    				<h1 class="name-cube">Jules THUYSBAERT</h1>
+	    				<img class="img-us" src="./app/images/Jules.png" alt="img-jules">
+	    				<p>Développement du backend</p>
+	    			</div>
+	    		</div>
+    		</div>
+	    </div>
+	    </div>
     `; 
     console.log("Section content:", section.innerHTML);
     } else {
