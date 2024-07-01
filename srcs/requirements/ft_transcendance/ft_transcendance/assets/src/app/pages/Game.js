cc7036de5dcd3da8052ce4d51d88c43e3444d466
hucoulon <ryze.engan@gmail.com>
2024-07-01 17:12:57 +0200

    changement de game.js

--- srcs/requirements/ft_transcendance/ft_transcendance/assets/src/app/pages/Game.js
+++ srcs/requirements/ft_transcendance/ft_transcendance/assets/src/app/pages/Game.js
@@ -5,25 +5,44 @@ const Game = () => {
         section.innerHTML = 
     `   
         <div class="game-container" id="game-container">
-            <canvas class="game-canvas" id="game-canvas"></canvas>
-            <div class="button-container initial-buttons" id="initial-buttons">
-                <button class="game-button" id="local-mode">Local</button>
-                <button class="game-button" id="tournoi-mode">Tournoi</button>
-            </div>
-            <div class="button-container tournoi-buttons" id="tournoi-buttons">
-                <button class="game-button" id="three-players-btn">3 Players</button>
-                <button class="game-button" id="four-players-btn">4 Players</button>
-                <button class="game-button" id="five-players-btn">5 Players</button>
-            </div>
-            <div class="session-name-input hidden" id="session-name-input">
-                <input type="text" class="session-name" id="session-name" placeholder="Nom de la session">
-                <button class="game-button submit-btn" id="submit-session-name">Submit</button>
-            </div>
-            <div id="player-names" style="display: none;">
-                <form id="player-names-form"></form>
-                <button id="submit-players-btn">Submit</button>
-            </div>
+            <canvas id="game-canvas" width="1200" height="700"></canvas>
+        <div class="button-container" id="initial-buttons">
+            <button class="game-button" id="local-mode">Local</button>
+            <button class="game-button" id="tournoi-mode">Tournoi</button>
         </div>
+        <div class="button-container" id="settings">
+            <div>
+                <label for="ball-speed">Ball Speed:</label>
+                <select id="ball-speed">
+                    <option value="15">Slow</option>
+                    <option value="20">Normal</option>
+                    <option value="25">Fast</option>
+                </select>
+            </div>
+            <div>
+                <label for="paddle-speed">Paddle Speed:</label>
+                <select id="paddle-speed">
+                    <option value="5">Slow</option>
+                    <option value="7">Normal</option>
+                    <option value="10">Fast</option>
+                </select>
+            </div>
+            <div>
+                <label for="background-color">Background Color:</label>
+                <select id="background-color">
+                    <option value="black">Black</option>
+                    <option value="blue">Blue</option>
+                    <option value="green">Green</option>
+                </select>
+            </div>
+            <button class="game-button" id="start-game-btn">Start Game</button>
+        </div>
+        <div class="button-container" id="tournoi-buttons">
+            <button class="game-button" id="three-players-btn">3 Players</button>
+            <button class="game-button" id="four-players-btn">4 Players</button>
+            <button class="game-button" id="five-players-btn">5 Players</button>
+        </div>
+    </div>
     `;
 
         const canvas = document.getElementById('game-canvas');
@@ -34,8 +53,6 @@ const Game = () => {
         let player2Y = canvas.height / 2;
         let ballX = canvas.width / 2;
         let ballY = canvas.height / 2;
-        let ballSpeedX = 0;
-        let ballSpeedY = 0;
         let scorePlayer1 = 0;
         let scorePlayer2 = 0;
 
@@ -44,25 +61,55 @@ const Game = () => {
         console.log("inside Game");
 
         // Fonction pour dessiner le jeu
-        function drawGame() {
+        function drawGameTournoi(player1_name, player2_name) {
             context.clearRect(0, 0, canvas.width, canvas.height);
-
+        
             // Dessiner les joueurs
             context.fillStyle = 'white';
             context.fillRect(20, player1Y, 10, 70);
             context.fillRect(canvas.width - 20, player2Y, 10, 70);
-
+        
             // Dessiner la balle
             context.fillRect(ballX, ballY, 10, 10);
-
+        
+            // Afficher le score
+            context.fillStyle = 'green';
+            context.font = '24px Arial';
+            context.fillText(`${player1_name}: ${scorePlayer1}`, 20, 30);
+            context.fillText(`${player2_name}: ${scorePlayer2}`, canvas.width - 150, 30);
+        }
+        
+        
+        function drawGame() {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+        
+            // Dessiner les joueurs
+            context.fillStyle = 'white';
+            context.fillRect(20, player1Y, 10, 70);
+            context.fillRect(canvas.width - 20, player2Y, 10, 70);
+        
+            // Dessiner la balle
+            context.fillRect(ballX, ballY, 10, 10);
+        
             // Afficher le score
             context.fillStyle = 'green';
             context.font = '24px Arial';
             context.fillText(`Player 1: ${scorePlayer1}`, 20, 30);
             context.fillText(`Player 2: ${scorePlayer2}`, canvas.width - 150, 30);
         }
+        
 
         // Mettre à jour le jeu
+        function updateGameTournoi(state, player1_name, player2_name) {
+            player1Y = state.player1_y_position;
+            player2Y = state.player2_y_position;
+            ballX = state.ball_x_position;
+            ballY = state.ball_y_position;
+            scorePlayer1 = state.score_player1;
+            scorePlayer2 = state.score_player2;
+            drawGameTournoi(player1_name, player2_name);
+        }
+        
         function updateGame(state) {
             player1Y = state.player1_y_position;
             player2Y = state.player2_y_position;
@@ -73,17 +120,22 @@ const Game = () => {
             drawGame();
         }
 
-        function connectWebSocket(roomName) {
+        function connectWebSocket(roomName, ballSpeed, paddleSpeed) {
             if (socket) {
                 socket.close();
             }
-
-            socket = new WebSocket(`ws://${window.location.host}/api/game/${roomName}/`);
-
+        
+            socket = new WebSocket(`ws://${window.location.host}/game/${roomName}/`);
+        
             socket.onopen = function(event) {
                 console.log('Connected to the server');
+                socket.send(JSON.stringify({
+                    'type': 'config',
+                    'ball_speed': ballSpeed,
+                    'paddle_speed': paddleSpeed,
+                }));
             };
-
+        
             socket.onmessage = function(event) {
                 const data = JSON.parse(event.data);
                 if (data.type === 'game_state') {
@@ -95,26 +147,86 @@ const Game = () => {
                     alert(`Player ${data.winner} wins!`);
                     socket.close();
                     socket = null;
-                    hideAll();
-                    showInitialMenu();
+                    window.hideAll();
+                    window.showInitialMenu();
                 }
             };
-
+        
             socket.onclose = function(event) {
                 console.log('Disconnected from the server');
             };
-
+        
             socket.onerror = function(error) {
                 console.error(`WebSocket error: ${error.message}`);
             };
-
+        
             document.addEventListener('keydown', function(event) {
                 keys[event.code] = true;
                 handleKeys();
             });
-
+        
             document.addEventListener('keyup', function(event) {
                 keys[event.code] = false;
+            });
+        }
+        
+        function connectWebSocketTournoi(roomName, player1_name, player2_name, nextRoundParticipants, ballSpeed, paddleSpeed) {
+            if (socket) {
+                socket.close();
+            }
+        
+            return new Promise((resolve, reject) => {
+                socket = new WebSocket(`ws://${window.location.host}/game/${roomName}/`);
+        
+                socket.onopen = function(event) {
+                    console.log('Connected to the server');
+                    // socket.send(JSON.stringify({
+                    //     'type': 'config',
+                    //     'ball_speed': ballSpeed,
+                    //     'paddle_speed': paddleSpeed,
+                    // }));
+                };
+        
+                socket.onmessage = function(event) {
+                    const data = JSON.parse(event.data);
+                    if (data.type === 'game_state') {
+                        updateGameTournoi(data, player1_name, player2_name);
+                    } else if (data.type === 'start_game') {
+                        console.log('The game has started!');
+                    } else if (data.type === 'game_over') {
+                        console.log("Fin de game");
+                        let winner;
+                        if (data.winner == 1) {
+                            winner = player1_name;
+                        } else {
+                            winner = player2_name
+                        }
+                        alert(`${player1_name} vs ${player2_name} - Winner: ${winner}`);
+                        nextRoundParticipants.push(winner);
+                        socket.close();
+                        socket = null;
+                        window.hideAll();
+                        resolve();
+                    }
+                };
+        
+                socket.onclose = function(event) {
+                    console.log('Disconnected from the server');
+                };
+        
+                socket.onerror = function(error) {
+                    console.error(`WebSocket error: ${error.message}`);
+                    reject(error);
+                };
+        
+                document.addEventListener('keydown', function(event) {
+                    keys[event.code] = true;
+                    handleKeys();
+                });
+        
+                document.addEventListener('keyup', function(event) {
+                    keys[event.code] = false;
+                });
             });
         }
 
@@ -148,30 +260,30 @@ const Game = () => {
         // Menu Functions
         const initialButtons = document.getElementById("initial-buttons");
         const tournoiButtons = document.getElementById("tournoi-buttons");
-        const sessionNameInput = document.getElementById("session-name-input");
         const localModeButton = document.getElementById("local-mode");
         const localModeTournoi = document.getElementById("tournoi-mode");
-        const playerNamesDiv = document.getElementById('player-names');
-        const playerNamesForm = document.getElementById('player-names-form');
-        const submitPlayersButton = document.getElementById('submit-players-btn');
-        const submitSessionNameButton = document.getElementById("submit-session-name");
-        const sessionName = document.getElementById("session-name");
+        const settings = document.getElementById("settings");
+        const startGameButton = document.getElementById("start-game-btn");
+        const ballSpeed = document.getElementById("ball-speed");
+        const paddleSpeed = document.getElementById("paddle-speed");
+        const backgroundColor = document.getElementById("background-color");
 
         let playerNames = [];
-        let currentMatchIndex = 0;
+
+        showInitialMenu();
+        console.log("inside Game_menu")
 
         function hideAll() {
             initialButtons.classList.add("hidden");
             tournoiButtons.classList.add("hidden");
-            playerNamesDiv.style.display = 'none';
-            sessionNameInput.style.display = 'none';
+            settings.classList.add("hidden");
         }
 
         function showInitialMenu() {
-            sessionName.value = "";
-            drawInitialGame();
+            drawInitialGame()
             hideAll();
-            initialButtons.classList.remove("hidden");
+            settings.classList.remove("hidden");
+            // initialButtons.classList.remove("hidden");
         }
 
         function drawInitialGame() {
@@ -192,40 +304,31 @@ const Game = () => {
             context.fillText(`Player 2: 0`, canvas.width - 150, 30);
         }
 
+        startGameButton.addEventListener("click", function() {
+            console.log(backgroundColor.value);
+            console.log(ballSpeed.value);
+            console.log(paddleSpeed.value);
+            document.getElementById('game-canvas').style.backgroundColor = backgroundColor.value;
+            hideAll();
+            initialButtons.classList.remove("hidden");
+            
+        });
+    
         localModeButton.addEventListener("click", function() {
             console.log("inside local");
             hideAll();
-            sessionNameInput.style.display = 'flex';
-            submitSessionNameButton.onclick = function() {
-                const roomName = "local_" + sessionName.value;
-                console.log("inside input");
-                if (roomName) {
-                    hideAll();
-                    connectWebSocket(roomName);
-                }
-            };
+            const roomName = "local_" + new Date().getTime();
+            console.log(roomName);
+            hideAll();
+            connectWebSocket(roomName, ballSpeed.value, paddleSpeed.value);
         });
 
         document.getElementById('three-players-btn').addEventListener('click', () => promptPlayerNames(3));
         document.getElementById('four-players-btn').addEventListener('click', () => promptPlayerNames(4));
         document.getElementById('five-players-btn').addEventListener('click', () => promptPlayerNames(5));
 
-        submitPlayersButton.addEventListener('click', function() {
-            players = [];
-            const formData = new FormData(playerNamesForm);
-            for (let [name, value] of formData.entries()) {
-                players.push(value);
-            }
-            if (players.length < 2) {
-                alert('Please enter names for all players.');
-                return;
-            }
-            hideAll();
-            startTournament();
-        });
-
         function promptPlayerNames(numPlayers) {
-            console.log("inside prompte player name");
+            console.log("inside prompte player name")
             playerNames = [];
             for (let i = 1; i <= numPlayers; i++) {
                 const playerName = prompt(`Player ${i} name:`);
@@ -236,40 +339,53 @@ const Game = () => {
                     i--; // Repeat the prompt for this player
                 }
             }
-            startTournament(playerNames);
+            for (let j = 0; j < numPlayers; j++) {
+                console.log("player name :", playerNames[j]);
+            }
+            runTournament(playerNames);
         }
-
+    
         localModeTournoi.addEventListener("click", function() {
             console.log("inside tournoi");
             hideAll();
+            // settings.classList.remove("hidden");
             tournoiButtons.classList.remove("hidden");
         });
-
-        function startTournament(players) {
-            if (players.length <= 1) {
-                alert(`Tournament Over! Winner: ${players[0]}`);
-                showInitialMenu();
-                return;
+    
+        async function runTournament(participants) {
+            let round = 1;
+        
+            while (participants.length > 1) {
+                console.log(`Round ${round}:`);
+                let nextRoundParticipants = [];
+        
+                // Si le nombre de participants est impair, un participant avance automatiquement
+                if (participants.length % 2 !== 0) {
+                    let luckyParticipant = participants.pop();
+                    nextRoundParticipants.push(luckyParticipant);
+                    console.log(`${luckyParticipant} advances to the next round automatically`);
+                    // Wait for a short time to simulate real-time progression
+                    // await new Promise(resolve => setTimeout(resolve, 1000));
+                }
+        
+                // Faire s'affronter les participants par paires
+                for (let i = 0; i < participants.length; i += 2) {
+                    let player1 = participants[i];
+                    let player2 = participants[i + 1];
+                    const roomName = `local_match_${player1}_vs_${player2}`.replace(/[^a-zA-Z0-9_]/g, '');
+                    hideAll();
+                    alert(`Round: ${round}  Match: ${player1} Vs ${player2}`);
+                    await connectWebSocketTournoi(roomName, player1, player2, nextRoundParticipants, ballSpeed.value, paddleSpeed.value)
+                }
+        
+                // Préparer les participants pour le prochain round
+                participants = nextRoundParticipants;
+                round++;
             }
-            currentMatchIndex = 0;
-            nextMatch(currentMatchIndex);
-        }
-
-        function nextMatch(currentMatchIndex) {
-            if (playerNames.length <= 1) {
-                alert(`Tournament is Over! Winner: ${playerNames[0]}`);
-                showInitialMenu();
-                return;
-            }
-
-            const player1 = playerNames[currentMatchIndex];
-            const player2 = playerNames[(currentMatchIndex + 1) % playerNames.length];
-
-            alert(`Next Match: ${player1} vs ${player2}`);
-            currentMatchIndex += 1;
-            const roomName = "local_" + `match_${currentMatchIndex}`;
+        
+            console.log(`Tournament Winner: ${participants[0]}`);
             hideAll();
-            connectWebSocket(roomName);
+            showInitialMenu();
         }
 
         window.hideAll = hideAll;
--- srcs/requirements/ft_transcendance/ft_transcendance/pong/routing.py
+++ srcs/requirements/ft_transcendance/ft_transcendance/pong/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'api/game/(?P<room_name>\w+)/$', consumers.PongConsumer.as_asgi()),
-    re_path(r'api/local_game/(?P<room_name>\w+)/$', consumers.PongConsumer.as_asgi()),
+    re_path(r'game/(?P<room_name>\w+)/$', consumers.PongConsumer.as_asgi()),
+    re_path(r'local_game/(?P<room_name>\w+)/$', consumers.PongConsumer.as_asgi()),
 ]
