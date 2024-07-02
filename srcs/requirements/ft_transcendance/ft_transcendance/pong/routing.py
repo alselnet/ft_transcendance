6a042a3a4b68ab86e07bfe9d196392ff37e94418
hucoulon <ryze.engan@gmail.com>
2024-07-02 15:00:04 +0200

    merging jeu front / eu back probleme bundle

--- srcs/requirements/ft_transcendance/ft_transcendance/pong/routing.py
+++ srcs/requirements/ft_transcendance/ft_transcendance/pong/routing.py
@@ -3,5 +3,4 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'game/(?P<room_name>\w+)/$', consumers.PongConsumer.as_asgi()),
-    re_path(r'local_game/(?P<room_name>\w+)/$', consumers.PongConsumer.as_asgi()),
 ]
